4139385ad38f7e76975c712a7203280b67a90ef6
JaldeepSenjaliya <jaldeepsenjaliya@gmail.com>
2022-07-14 19:24:36 -0400

    Created ListView and Added Info Icon

--- App.js
+++ App.js
@@ -2,56 +2,67 @@
 //Make sure this stays at the very top and nothing comes before
 //import 'react-native-gesture-handler';
 
-import { StyleSheet, View, Text, Pressable, Image, FlatList} from 'react-native';
+import { StyleSheet, View, SafeAreaView, Text, Pressable, Image, FlatList} from 'react-native';
 
 
-function list() {
- const items = [1, 2, 3, 4, 5] 
- }
+// function list() {
+//  const items = [1, 2, 3, 4, 5] 
+//  }
 
 export default function App() {
 
   const handlePress = () => console.log("Text Pressed!");
   return (
     <SafeAreaView style={styles.listContainer}>
-      <FlatList>
         <View style={styles.locationContainer}>
           <Text style={styles.locationText}>Compassion Meals</Text>
-        </View>
-        <View style={styles.distanceContainer}>
+       </View>
+       <View style={styles.distanceContainer}>
           <Text style={styles.distanceText}>2.00mi</Text>
-        </View>
+        </View>    
         <Pressable onPress={handlePress} style={styles.infoBtn}>
           <View>
-              <Image source={require('')}/>
+              <Image source={require('./assets/info.png')} style={{width: 20, height: 20, tintColor: 'white'}} />
           </View>
         </Pressable>
-
-      </FlatList>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   listContainer: {
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+    flexDirection: 'row'
   },
   locationContainer: {
-    flex: 1,
-    backgroundColor: "black"
+    //flex: 1,
+    backgroundColor: 'black',
+    paddingLeft: 5,
+    paddingTop: 5,
+    paddingBottom: 5
   },
   locationText: {
     color: 'white'
   },
   distanceContainer: {
-    backgroundColor: 'grey',
-    flex: 0.5
+    // backgroundColor: 'grey',
+    // paddingLeft: 5,
+    // paddingTop: 5,
+    // paddingBottom: 5
   },
   distanceText: {
-    color: '#B2FFC3'
+    color: '#B2FFC3',
+    backgroundColor: 'grey',
+    paddingLeft: 5,
+    paddingTop: 5,
+    paddingBottom: 5
+
   },
   infoBtn: {
-    flex: 0.5
+    //flex: 0.5
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5
   },
 });
 
